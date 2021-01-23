3aca16b1fbe5cf7971a709508304dbaa2655ac72
Mustafa Faruk Esen <mustafarukesen@gmail.com>
2021-01-23 03:10:00 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T1, T2, T3>
+    {
+        T1[] key;
+        T1[] tempKey;
+
+        T2[] value;
+        T2[] tempValue;
+
+        T3[] value2;
+        T3[] tempValue2;
+
+        public MyDictionary()
+        {
+            key = new T1[0];
+            value = new T2[0];
+            value2 = new T3[0];
+        }
+
+        public void Add(T1 keys, T2 values, T3 values2)
+        {
+            tempKey = key;
+            tempValue = value;
+            tempValue2 = value2;
+
+            key = new T1[key.Length+1];
+            value = new T2[value.Length+1];
+            value2 = new T3[value2.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                key[i] = tempKey[i];
+                value[i] = tempValue[i];
+                value2[i] = tempValue2[i];
+            }
+
+            key[key.Length-1] = keys;
+            value[value.Length-1] = values;
+            value2[value2.Length - 1] = values2;
+        }
+
+        public int Count
+        {
+            get { return key.Length;  }
+        }
+
+        public T1[] Key
+        {
+            get { return key; }
+        }
+
+        public T2[] Value
+        {
+            get { return value; }
+        }
+
+        public T3[] Value2
+        {
+            get { return value2; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string, double> Exam = new MyDictionary<string, string, double>();
+            Exam.Add("Monday", "Math1", 22.15);
+            Exam.Add("Tuesday", "Phy1", 11.25);
+            Exam.Add("Wednesday", "ICE",21.35);
+            Exam.Add("Thursday", "LinAlg",10.35);
+            Exam.Add("Friday", "Eng",15.45);
+            Exam.Add("Saturday", "Web",18.15);
+
+            for (int i = 0; i < Exam.Count; i++)
+            {
+                Console.WriteLine("Exam days= " + Exam.Key[i] + "\t\tExam date= " + Exam.Value2[i] + "\t\tExams= " + Exam.Value[i] );
+            }
+
+
+            //foreach (var exams in Exam.Key)
+            //{
+            //    Console.WriteLine(exams);
+            //}
+
+            //foreach (var exams in Exam.Value2)
+            //{
+            //    Console.WriteLine(exams);
+            //}
+
+            //foreach (var exams in Exam.Value)
+            //{
+            //    Console.WriteLine(exams);
+            //}
+
+        }
+    }
+}
